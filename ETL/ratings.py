aaa68a0b2fc891f625de6cd369c1e87e919429f7
Morapakala, Sai Hari Chandan <saimorap@iu.edu>
2023-06-19 04:06:08 -0400

    added rating script along with adding its path to the rest API

---
+++ ETL/ratings.py
@@ -0,0 +1,195 @@
+from sqlalchemy import create_engine
+import psycopg2
+import pandas as pd
+
+import numpy as np
+from scipy.stats import rankdata
+
+user = "user"
+password = "password"
+host = "192.168.59.101"
+port = "30432"
+database = "football-db"
+
+# Database Configurations
+# database = os.environ["database"]
+# user = os.environ["user"]
+# password = os.environ["password"]
+# host = os.environ["host"]
+# port = os.environ["port"]
+
+
+def pushToDB(table_name, df, conn, if_exists="replace", index=False):
+    """
+    Pushes the DataFrame to a specified table.
+
+    Args:
+        table_name (str): Name of the resulting table in database.
+        df (pd.DataFrame): DataFrame to uploaded to database.
+        conn (sqlalchemy.engine.base.Connection): Connection engine to the database.
+        if_exists (str, optional): How to behave if the table already exists.
+
+            * fail: Raise a ValueError.
+            * replace: Drop the table before inserting new values.
+            * append: Insert new values to the existing table.
+
+            Defaults to "replace".
+
+        index (bool, optional): Write DataFrame index as a column with column name "index".
+
+            Defaults to False.
+    """
+
+    # Begin a transaction
+    transaction = conn.begin()
+
+    try:
+        # Push the DataFrame to PostgreSQL
+        df.to_sql(name=table_name, con=conn, if_exists=if_exists, index=index)
+
+        # Commit the transaction
+        transaction.commit()
+        print(f"{table_name} has been successfully committed.")
+
+    except Exception as e:
+        # Rollback the transaction if there's an error
+        transaction.rollback()
+
+        print(
+            f"Error occurred in uploading {table_name}. Transaction has been rolled back."
+        )
+        print(f"Error message: {str(e)}")
+        raise (e)
+
+
+def inverse_percentile_rank(data, min_percentile=35, max_percentile=100):
+    ranks = rankdata(data)
+    min_rank = np.min(ranks)
+    max_rank = np.max(ranks)
+    scaled_ranks = (
+        (max_percentile - min_percentile)
+        * (1 - (ranks - min_rank) / (max_rank - min_rank))
+    ) + min_percentile
+    return scaled_ranks
+
+
+def percentile_rank(data, min_percentile=40, max_percentile=100):
+    ranks = rankdata(data)
+    min_rank = np.min(ranks)
+    max_rank = np.max(ranks)
+    scaled_ranks = ((ranks - min_rank) / (max_rank - min_rank)) * (
+        max_percentile - min_percentile
+    ) + min_percentile
+    return scaled_ranks
+
+
+def get_attack_data(conn):
+    attack = pd.read_sql_query(
+        sql="""SELECT t1.squad, standard_gls, standard_sot, poss FROM shooting_stats as t1 
+                    INNER JOIN standard_stats as t2 ON t1.squad = t2.squad 
+                                                        AND t1.value = t2.value where t1.value = 'squad' """,
+        con=conn,
+    )
+
+    attack[["standard_gls", "standard_sot", "poss"]] = attack[
+        ["standard_gls", "standard_sot", "poss"]
+    ].apply(percentile_rank)
+
+    attack.loc[:, "attack"] = attack[["standard_gls", "standard_sot", "poss"]].mean(
+        axis=1
+    )
+
+    return attack
+
+
+def get_defence_data(conn):
+    defence = pd.read_sql_query(
+        sql="""SELECT t1.squad, int, tackles_tkl, tackles_tklw, performance_ga, performance_sota FROM defensive_action_stats as t1 
+                  INNER JOIN goalkeeping_stats as t2 ON t1.squad = t2.squad 
+                                                    AND t1.value = t2.value where t1.value = 'squad' """,
+        con=conn,
+    )
+
+    defence.loc[:, "tackles_win_pct"] = (
+        defence.loc[:, "tackles_tklw"].astype("float16")
+        / defence.loc[:, "tackles_tkl"].astype("float16")
+    ) * 100
+
+    defence[["tackles_win_pct", "int"]] = defence[["tackles_win_pct", "int"]].apply(
+        percentile_rank
+    )
+    defence[["performance_ga", "performance_sota"]] = defence[
+        ["performance_ga", "performance_sota"]
+    ].apply(inverse_percentile_rank)
+
+    defence.loc[:, "defence"] = defence[
+        ["tackles_win_pct", "int", "performance_ga", "performance_sota"]
+    ].mean(axis=1)
+
+    return defence
+
+
+def get_midfield_data(conn):
+    midfield = pd.read_sql_query(
+        sql="""SELECT t1.squad, passtypes_crs, outcomes_off, outcomes_blocks, ast, kp FROM passing_types_stats as t1 
+                  INNER JOIN passing_stats as t2 ON t1.squad = t2.squad 
+                                                    AND t1.value = t2.value where t1.value = 'squad' """,
+        con=conn,
+    )
+
+    midfield[["passtypes_crs", "ast", "kp"]] = midfield[
+        ["passtypes_crs", "ast", "kp"]
+    ].apply(percentile_rank)
+    midfield[["outcomes_off", "outcomes_blocks"]] = midfield[
+        ["outcomes_off", "outcomes_blocks"]
+    ].apply(inverse_percentile_rank)
+
+    midfield.loc[:, "midfield"] = midfield[
+        ["outcomes_off", "outcomes_blocks", "passtypes_crs", "ast", "kp"]
+    ].mean(axis=1)
+    return midfield
+
+
+def merge_data(attack, midfield, defence):
+    try:
+        return pd.merge(
+            attack[["squad", "attack"]], midfield[["squad", "midfield"]], on="squad"
+        ).merge(defence[["squad", "defence"]], on="squad")
+    except Exception as e:
+        print("Unable to merge Attack, Midfield and Defence dataframes....")
+        raise (e)
+
+
+if __name__ == "__main__":
+    # Connect to DB
+    try:
+        print("Establishing Connection with DB....")
+        db = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
+        conn = db.connect()
+        print("Successfully Established Connection with DB....")
+
+    except Exception as e:
+        print("Unable to Establish Connection with DB....")
+        raise (e)
+
+    # Get Attack, Midfield and Defence data
+    attack = get_attack_data(conn=conn)
+    print("Getting Attack Data and Calculating Ratings...")
+    midfield = get_midfield_data(conn=conn)
+    print("Getting Midfield Data and Calculating Ratings...")
+    defence = get_defence_data(conn=conn)
+    print("Getting Defence Data and Calculating Ratings...")
+
+    # Merging the data
+    data = merge_data(attack=attack, midfield=midfield, defence=defence)
+
+    # Calculating the overall rating
+    data.loc[:, "overall"] = data[["attack", "midfield", "defence"]].mean(axis=1)
+
+    # Pushing to DB
+    pushToDB(table_name="ratings", df=data, conn=conn)
+
+    # Close the connection
+    conn.close()
+
+    print("Rating Successfully Loaded....")
--- RestApi/main.go
+++ RestApi/main.go
@@ -6,8 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/iancoleman/strcase"
+	"strings"
 
 	_ "github.com/lib/pq"
 
@@ -25,6 +24,51 @@ const (
 	host     = "192.168.59.101"
 	port     = "30432"
 )
+
+func runQuery(rows *sql.Rows) (interface{}, error) {
+
+	// Create a map to hold the rows
+	stats := []map[string]interface{}{}
+
+	// Iterate over the rows
+	for rows.Next() {
+
+		// Get column names
+		columns, err := rows.Columns()
+		if err != nil {
+			return nil, err
+		}
+
+		// Create a slice to hold column values
+		values := make([]interface{}, len(columns))
+		columnPointers := make([]interface{}, len(columns))
+		for i := range columns {
+			columnPointers[i] = &values[i]
+		}
+
+		// Scan the row values into the columnPointers slice
+		err = rows.Scan(columnPointers...)
+		if err != nil {
+			return nil, err
+		}
+
+		// Create a map to hold the row data
+		rowData := map[string]interface{}{}
+		for i, column := range columns {
+			rowData[column] = values[i]
+		}
+
+		// Append the row data to the stats slice
+		stats = append(stats, rowData)
+	}
+
+	// Check for errors during iteration
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return stats, nil
+}
 
 func main() {
 
@@ -51,68 +95,68 @@ func main() {
 	fmt.Println("Successfully connected to database!")
 
 	router := gin.Default()
-	router.GET("/PL/:team", app.GetTeamPerformance)
+	router.GET("/PL/stats/:team", app.GetTeamStats)
+	router.GET("/PL/ratings/:team", app.GetTeamRatings)
 	router.Run("localhost:8080")
 }
 
-func (app *App) GetTeamPerformance(c *gin.Context) {
+func (app *App) GetTeamStats(c *gin.Context) {
 	team := c.Param("team")
 
-	fmt.Println("Querying For:", strcase.ToCamel(team))
+	fmt.Println("Querying Stats For:", strings.ToLower(team))
 
-	// rows, err := app.DB.Query(`Select * from regular_season where "Squad" = $1`, strcase.ToCamel(team))
-	query := fmt.Sprintf(`Select * from regular_season where "Squad" = '%s';`, strcase.ToCamel(team))
-	fmt.Println(query)
-	rows, err := app.DB.Query(query)
+	// Run query and Get Data
+	rows, err := app.DB.Query(`Select * from regular_season where squad = $1`, strings.ToLower(strings.ReplaceAll(team, "'", "''")))
 	if err != nil {
-
-		c.JSON(500, gin.H{"error": "Failed to retrieve data"})
-		return
-	}
-	defer rows.Close()
-
-	// Create a map to hold the rows
-	users := []map[string]interface{}{}
-
-	// Iterate over the rows
-	for rows.Next() {
-
-		// Get column names
-		columns, err := rows.Columns()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Create a slice to hold column values
-		values := make([]interface{}, len(columns))
-		columnPointers := make([]interface{}, len(columns))
-		for i := range columns {
-			columnPointers[i] = &values[i]
-		}
-
-		// Scan the row values into the columnPointers slice
-		err = rows.Scan(columnPointers...)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Create a map to hold the row data
-		rowData := map[string]interface{}{}
-		for i, column := range columns {
-			rowData[column] = values[i]
-		}
-
-		// Append the row data to the users slice
-		users = append(users, rowData)
+		log.Fatal(err)
+		c.IndentedJSON(http.StatusInternalServerError, "Failed to Query Data")
 	}
 
-	// Check for errors during iteration
-	if err = rows.Err(); err != nil {
+	data, err := runQuery(rows)
+	if err != nil {
+		log.Fatal(err)
+		c.IndentedJSON(http.StatusInternalServerError, "Data Error")
+	}
+
+	rows.Close()
+
+	// Convert the data slice to JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Convert the users slice to JSON
-	jsonData, err := json.Marshal(users)
+	// Set the response content type to JSON
+	c.Header("Content-Type", "application/json")
+
+	// Send the JSON data as the response
+	c.IndentedJSON(http.StatusOK, string(jsonData))
+}
+
+func (app *App) GetTeamRatings(c *gin.Context) {
+	team := c.Param("team")
+
+	fmt.Println("Querying Ratings For:", strings.ToLower(team))
+
+	// Run query and Get Data
+	rows, err := app.DB.Query(`Select * from ratings where squad = $1`, strings.ToLower(team))
+	if err != nil {
+		log.Fatal(err)
+		c.IndentedJSON(http.StatusInternalServerError, "Failed to Query Data")
+	}
+
+	data, err := runQuery(rows)
+	if err != nil {
+		log.Fatal(err)
+		c.IndentedJSON(http.StatusInternalServerError, "Data Error")
+	}
+
+	rows.Close()
+
+	fmt.Println(data)
+
+	// Convert the data slice to JSON
+	jsonData, err := json.Marshal(data)
 	if err != nil {
 		log.Fatal(err)
 	}
